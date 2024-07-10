b140d181b196d371abbbcc167b97bee0c43e10fd
Nuno Conceicao <89638876+concenun@users.noreply.github.com>
2024-07-10 11:02:21 +0100

    feat: improve css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import {ListComponent} from "./list/list.component";
 import {TodoItem} from "../types/todoItem";
 import {AddComponent} from "./add/add.component";
 import {TodoItemStorageService} from "../services/todoItemStorageService";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ListComponent, AddComponent],
+  imports: [RouterOutlet, ListComponent, AddComponent, MatIcon],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
